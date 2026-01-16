e096f2a3e30edc36ee54ce6821a81aa2dd96da42
bridevmx <bridevmx@gmail.com>
2026-01-16 12:03:28 -0600

    update 1.1

--- start.sh
+++ start.sh
@@ -9,7 +9,7 @@ echo "--- Iniciando despliegue en Shiper con DragonflyDB ---"
 # Si subes esto, tu contenedor podría morir por falta de RAM.
 # --bind localhost: Solo escucha dentro del contenedor por seguridad.
 echo "Iniciando DragonflyDB (Límite estricto: 64MB)..."
-/usr/local/bin/dragonfly --maxmemory=64mb --cache_mode=true --bind localhost &
+/usr/local/bin/dragonfly --maxmemory=256mb --cache_mode=true --bind localhost &
 
 # Esperamos 3 segundos para asegurar que Dragonfly esté listo para recibir conexiones
 sleep 3
