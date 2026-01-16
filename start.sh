38b1199838d6771a7298ffc333c206320fd85da0
bridevmx <bridevmx@gmail.com>
2026-01-16 11:15:41 -0600

    refactor: Migra la base de datos en contenedor de DragonflyDB a Redis y optimiza la instalación de dependencias de Node.js.

--- start.sh
+++ start.sh
@@ -1,20 +1,13 @@
 #!/bin/bash
-set -e # Detener el script si algo falla
+echo "--- Iniciando Modo Supervivencia (Redis) ---"
 
-echo "--- Iniciando despliegue en Shiper con DragonflyDB ---"
+# Iniciamos Redis en modo demonio (background)
+# maxmemory 50mb: Vital para dejar espacio a Node.js
+redis-server --daemonize yes --maxmemory 50mb --maxmemory-policy allkeys-lru
 
-# 1. Iniciar DragonflyDB en segundo plano (& al final)
-# IMPORTANTE:
-# --maxmemory=64mb: Le damos solo 64MB. Dejamos ~180MB para Node.js.
-# Si subes esto, tu contenedor podría morir por falta de RAM.
-# --bind localhost: Solo escucha dentro del contenedor por seguridad.
-echo "Iniciando DragonflyDB (Límite estricto: 64MB)..."
-/usr/local/bin/dragonfly --maxmemory=64mb --cache_mode=true --bind localhost &
+# Esperamos un poco
+sleep 2
 
-# Esperamos 3 segundos para asegurar que Dragonfly esté listo para recibir conexiones
-sleep 3
-
-# 2. Iniciar tu servidor Fastify
-echo "Dragonfly listo. Iniciando Fastify..."
-# Usamos 'exec' para que Node pase a ser el proceso principal
+# Iniciamos Node
+echo "Iniciando Fastify..."
 exec node server.js
